57902a2a98493473cf1992a5269e69a84a0563b9
Ba Thanh <thanh.nguyenba@morsoftware.com>
2024-02-15 02:56:28 +0700

    Call API lazada

--- app/src/main/java/com/frank/demointer/network/ApiClient.kt
+++ app/src/main/java/com/frank/demointer/network/ApiClient.kt
@@ -1,5 +1,9 @@
 package com.frank.demointer.network
 
+import android.util.Log
+import com.google.gson.GsonBuilder
+import io.harkema.retrofitcurlprinter.Logger
+import io.harkema.retrofitcurlprinter.RetrofitCurlPrinterInterceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -24,24 +28,33 @@ object ApiClient {
 }
 
 object RetrofitLazadaClient {
-    private const val BASE_URL = "https://acs-m.lazada.vn"
+    private const val BASE_URL = "https://pdpdesc-m.lazada.vn"
 
     val retrofit: Retrofit by lazy {
         val interceptor = HttpLoggingInterceptor().apply {
             this.level = HttpLoggingInterceptor.Level.BODY
         }
         val client = OkHttpClient.Builder().apply {
-            this.addInterceptor(interceptor)
+            this.addInterceptor(RetrofitCurlPrinterInterceptor(object: Logger {
+                override fun log(message: String) {
+                    //info { message }
+                    Log.d("FrankAA", "Curl: $message")
+                }
+            }))
                 // time out setting
                 //.connectTimeout(3,TimeUnit.SECONDS)
                 //.readTimeout(20,TimeUnit.SECONDS)
                 //.writeTimeout(25,TimeUnit.SECONDS)
 
         }.build()
+
+        val gson = GsonBuilder()
+            .setLenient()
+            .create()
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(client)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
     }
 }
